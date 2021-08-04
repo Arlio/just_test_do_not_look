e50710a70eb88ff357b4f70dc5ad2be1ac128a72
Dmitry <dimkabizarre@yandex.ru>
2021-08-04 13:40:43 -0700

    Add template

--- main.go
+++ main.go
@@ -2,34 +2,42 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
 type User struct {
-	fname     string
-	lname     string
-	age       int8
-	happyness float32
+	Fname     string
+	Lname     string
+	Age       int8
+	Happyness float32
+	Langs     []string
 }
 
 func (u *User) getAllInfo() string {
 	return fmt.Sprintf(
 		"name: %v\nlastname: %v\nage: %v\nhap: %v\n",
-		u.fname, u.lname, u.age, u.happyness,
+		u.Fname, u.Lname, u.Age, u.Happyness,
 	)
 }
 
 func (u *User) setHappyness(newHappyness float32) {
-	u.happyness = newHappyness
+	u.Happyness = newHappyness
 }
 
 func homePage(w http.ResponseWriter, r *http.Request) {
 
-	user1 := User{"Dmitry", "Bakhmetyev", 26, .9}
+	user1 := User{"Dmitry", "Bakhmetyev", 26, .9, []string{
+		"JS", "PHP", "Python", "GO",
+	}}
 
 	user1.setHappyness(.4)
 
-	fmt.Fprintf(w, user1.getAllInfo())
+	//fmt.Fprintf(w, user1.getAllInfo())
+
+	tmpl, _ := template.ParseFiles("templates/home_page.html")
+
+	tmpl.Execute(w, user1)
 
 }
 
